b0930941377b3fd4898c3fa3a66556d931783fcc
p.venhryn <p.venhryn@pozitiff.ua>
2019-12-10 02:14:35 +0200

    update_site

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -6,6 +6,7 @@ exports.components = {
   "component---cache-dev-404-page-js": () => import("/home/pvenhryn/git/mag_cart-master/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/home/pvenhryn/git/mag_cart-master/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-blog-index-js": () => import("/home/pvenhryn/git/mag_cart-master/src/pages/blog/index.js" /* webpackChunkName: "component---src-pages-blog-index-js" */),
+  "component---src-pages-index-js": () => import("/home/pvenhryn/git/mag_cart-master/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-product-js": () => import("/home/pvenhryn/git/mag_cart-master/src/pages/product.js" /* webpackChunkName: "component---src-pages-product-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -9,6 +9,7 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/home/pvenhryn/git/mag_cart-master/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/pvenhryn/git/mag_cart-master/src/pages/404.js"))),
   "component---src-pages-blog-index-js": hot(preferDefault(require("/home/pvenhryn/git/mag_cart-master/src/pages/blog/index.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/pvenhryn/git/mag_cart-master/src/pages/index.js"))),
   "component---src-pages-product-js": hot(preferDefault(require("/home/pvenhryn/git/mag_cart-master/src/pages/product.js")))
 }
 
